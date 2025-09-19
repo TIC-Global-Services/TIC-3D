23655207a9cdb5a90c30a85f2633d95095cdad15
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-09-19 09:12:55 +0530

    scene added

--- components/Reusbale/Castle.tsx
+++ components/Reusbale/Castle.tsx
@@ -1,64 +1,27 @@
-'use client';
+"use client";
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+import { JSX } from "react";
 
-import React, { forwardRef, JSX } from 'react';
-import { useGLTF } from '@react-three/drei';
-import { Group } from 'three';
-import * as THREE from "three";
-
-
-type ModelProps = JSX.IntrinsicElements['group'];
-
-interface GLTFResult {
-  nodes: {
-    [key: string]: THREE.Mesh;
-  };
-  materials: {
-    [key: string]: THREE.Material;
-  };
-}
-const Model = forwardRef<Group, ModelProps>((props, ref) => {
-  const { nodes, materials } = useGLTF('/castle2.glb') as unknown as GLTFResult;
-
+// Castle component that works inside Canvas
+export function CastleModel(props: JSX.IntrinsicElements["group"]) {
+  const gltf = useGLTF("/castle2.glb");
+  
   return (
-    <group ref={ref} {...props} dispose={null}>
-      <group position={[-0.97, 30.245, 28.223]}>
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001.geometry} material={materials.Bark_Mat} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_1.geometry} material={materials.Brunches_Mat} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_2.geometry} material={materials['Material.007']} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_3.geometry} material={materials['MatID_1.002']} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_4.geometry} material={materials.MatID_1} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_5.geometry} material={materials.M_Castlewall} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_6.geometry} material={materials.M_Castleroof} />
-        <mesh castShadow receiveShadow geometry={nodes.Christmas_tree_4001_7.geometry} material={materials.M_Emissive} />
-      </group>
-
-      {/* Rock meshes using a loop to avoid repetition */}
-      {Array.from({ length: 7 }, (_, i) => (
-        <React.Fragment key={i}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={(nodes as any)[`Aset_rock_cliff_XL_ullqbb1ga_LOD200${i + 1}`].geometry}
-            material={materials['Material.007']}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={(nodes as any)[`Aset_rock_cliff_XL_ullqbb1ga_LOD200${i + 1}_1`].geometry}
-            material={materials['MatID_1.002']}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={(nodes as any)[`Aset_rock_cliff_XL_ullqbb1ga_LOD200${i + 1}_2`].geometry}
-            material={materials.MatID_1}
-          />
-        </React.Fragment>
-      ))}
+    <group {...props} dispose={null} position={[0, -50, 0]} // adjust to move model
+      scale={[3, 3, 3]}>
+      <primitive object={gltf.scene} />
+      <ambientLight intensity={0.4} color={"#ffffff"} />
+      <directionalLight
+        position={[20, 40, 20]}
+        intensity={1.2} // like exposure bump
+        color={"#ffdca8"}
+        castShadow
+      />
     </group>
   );
-});
+}
 
-useGLTF.preload('/castle2.glb');
-
-export default Model;
+// Main component with Canvas wrapper
+// Preload the model for better performance
+useGLTF.preload("/castle2.glb");
